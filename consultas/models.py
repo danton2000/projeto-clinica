f1e7fdffd5330df48e91fc5b7ee453c37e80f8d3
Danton Rodrigues <dantoncampos@hotmail.com>
2022-09-22 19:28:35 -0300

    alterando models.py

--- consultas/models.py
+++ consultas/models.py
@@ -71,6 +71,7 @@ class Medico(models.Model):
 
     especialidade = models.ForeignKey(
         Especialidades,
+        # ele não deixa que a especilidade seja apaga se ela já estiver vinculada a um medico
         on_delete=models.PROTECT,
         null= True,
         blank = True
